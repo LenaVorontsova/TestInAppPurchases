d486c5845a239331dce09be6431d274919693035
Elena Vorontsova <lena.voroncova26@yandex.ru>
2023-02-17 15:36:08 +0400

    create the project

---
+++ TestInAppPurchases/TestInAppPurchases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestInAppPurchases
+//
+//  Created by Lena Vorontsova on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
